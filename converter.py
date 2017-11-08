df80aa125d4adc44ccf7943f54362599617f50fa
ChristinaYu <xyu@unm.edu>
2017-11-07 22:26:26 -0700

    Added FFT and MFCC

--- converter.py
+++ converter.py
@@ -10,4 +10,9 @@ dirs = os.listdir(path)
 for f in dirs:
 	if f !='.DS_Store':
 		data, samplerate = sf.read(path+f)
-		outputWriter.writerow(data[:1000])
+		x=abs(scipy.fft(data)[:1000])
+		outputWriter.writerow(x[:1000])
+		ceps,mspec,spec=mfcc(data)
+		num_ceps=ceps.shape[0]
+		y=np.mean(ceps[int(num_ceps/10): int(num_ceps*9/10)],axis=0)
+		outputWriter2.writerow(y)
